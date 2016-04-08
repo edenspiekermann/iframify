fd55b80a5a3fd18fa41ee42ac39b00d04629a62b
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-04-08 16:31:14 +0200

    Cleaned up test suite

--- tests/index.js
+++ tests/index.js
@@ -5,157 +5,121 @@ describe('iframify', function () {
 
   it('should create an iframe', function () {
     var test = document.querySelector('.test-0 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-0 > iframe');
-
-    expect(iframe).to.be.ok();
+    var actual = iframify(test);
+    expect(actual).to.be.ok();
   });
 
   it('should inject content in iframe', function (done) {
     var test = document.querySelector('.test-1 > .iframify');
     var initialHTML = test.innerHTML;
-    iframify(test);
-    var iframe = document.querySelector('.test-1 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-
       var actual = initialHTML;
       var expected = iframe.contentDocument.body.innerHTML;
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import conserve inline styles', function (done) {
     var test = document.querySelector('.test-2 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-2 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = component.getAttribute('style');
       var expected = 'color: red';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import relevant styles from <style> tags', function (done) {
     var test = document.querySelector('.test-3 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-3 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('color');
       var expected = 'rgb(255, 0, 0)';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import relevant styles from external stylesheets', function (done) {
     var test = document.querySelector('.test-4 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-4 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('color');
       var expected = 'rgb(0, 255, 0)';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import styles for children', function (done) {
     var test = document.querySelector('.test-5 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-5 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component__element');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('color');
       var expected = 'rgb(0, 0, 255)';
-
       expect(actual).to.be.equal(expected);
-
       actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('font-weight');
       expected = 'bold';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import @media declarations', function (done) {
     var test = document.querySelector('.test-6 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-6 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('color');
       var expected = 'rgb(1, 33, 7)';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should import @supports declarations', function (done) {
     var test = document.querySelector('.test-7 > .iframify');
-    iframify(test);
-    var iframe = document.querySelector('.test-7 > iframe');
+    var iframe = iframify(test);
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('color');
       var expected = 'rgb(0, 42, 0)';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
 
   it('should allow passing extra CSS', function (done) {
     var test = document.querySelector('.test-8 > .iframify');
-    iframify(test, '.component--test-8 { font-style: italic }');
-    var iframe = document.querySelector('.test-8 > iframe');
+    var iframe = iframify(test, '.component--test-8 { font-style: italic }');
 
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var component = iframeDocument.querySelector('.component');
-
       var actual = iframe.contentWindow.getComputedStyle(component).getPropertyValue('font-style');
       var expected = 'italic';
-
       expect(actual).to.be.equal(expected);
-
       done();
     }
   });
@@ -167,12 +131,8 @@ describe('iframify', function () {
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var meta = iframeDocument.querySelector('meta[name="viewport"]');
-      console.log(meta)
-
       var actual = meta;
-
       expect(actual).to.be.ok();
-
       done();
     }
   });
@@ -184,12 +144,8 @@ describe('iframify', function () {
     iframe.onload = function () {
       var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
       var meta = iframeDocument.querySelector('meta[charset]');
-      console.log(meta)
-
       var actual = meta;
-
       expect(actual).to.be.ok();
-
       done();
     }
   });
